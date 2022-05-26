14ba2f878b6f23d99d8415fb9cdba5fdc013fde8
muhanad-alkhalil <muhanad.alhalil@gmail.com>
2022-05-26 21:32:57 +0300

    init

---
+++ database/migrations/2022_05_26_174123_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("subject");
+            $table->text("contetnt");
+            $table->boolean("isRead");
+
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('reciver_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
