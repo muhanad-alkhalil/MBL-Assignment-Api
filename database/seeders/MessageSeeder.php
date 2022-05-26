aeaa00b6dbfc41fd8d79327609f347c1a3bf134d
muhanad-alkhalil <muhanad.alhalil@gmail.com>
2022-05-26 22:46:12 +0300

    add seeders and create MessageController

---
+++ database/seeders/MessageSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class MessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('messages')->insert([
+            [
+                'subject' => "Hi Again",
+                'contetnt' => 'Just wanted to check on you',
+                'isRead' => 1,
+                'sender_id' => 2,
+                'reciver_id' => 1,
+            ],
+            [
+                'subject' => "Hi Friend",
+                'contetnt' => 'Just wanted to let you know I’ m good',
+                'isRead' => 0,
+                'sender_id' => 2,
+                'reciver_id' => 1,
+            ],
+            [
+                'subject' => "message1",
+                'contetnt' => 'this is message1',
+                'isRead' => 0,
+                'sender_id' => 2,
+                'reciver_id' => 1,
+            ],
+            [
+                'subject' => "title 1",
+                'contetnt' => 'aaaaaa aAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                'isRead' => 1,
+                'sender_id' => 2,
+                'reciver_id' => 1,
+            ],
+            [
+                'subject' => "message 2",
+                'contetnt' => 'Just wanted to check on you',
+                'isRead' => 0,
+                'sender_id' => 2,
+                'reciver_id ' => 1,
+            ],
+
+        ]);
+    }
+}
